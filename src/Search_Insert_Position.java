865905e254d0662db660b83cea16262de64376b2
Sarvar Masharipov <sarvarmasharipov72>
2021-12-30 18:47:09 +0300

    Complate Search Insert Position

---
+++ src/Search_Insert_Position.java
@@ -0,0 +1,11 @@
+public class Search_Insert_Position {
+    public int searchInsert(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == target)
+                return i;
+                else if (nums[i] > target)
+                return i;
+        }
+        return nums.length;
+    }
+}
