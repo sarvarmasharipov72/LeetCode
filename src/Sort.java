dd683e1484805ec11d41067a69bc3e881dbb261a
Sarvar Masharipov <sarvarmasharipov72>
2022-01-02 15:30:37 +0300

    Create Sort numbres class

---
+++ src/Sort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Sort {
+    public static int[] sort(int[] nums){
+        for (int i = 1; i < nums.length; i++){
+            if (nums[i] < nums[i - 1]){
+                int j = i;
+                while(true){
+                    if (j == 0) break;
+                    if (nums[j] > nums[j -1]) break;
+                    else{
+                        int temp = nums[j];
+                        nums[j] = nums[j-1];
+                        nums[j-1] = temp;
+                        j--;
+                    }
+                }
+            }
+        }
+        System.out.println("nums = " + Arrays.toString(nums));
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        sort(new int[]{2,1,4,9,5,7,0,3});
+    }
+}
