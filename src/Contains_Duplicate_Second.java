693cc4389faa9b26d95facf7771d5c637136280b
Sarvar Masharipov <sarvarmasharipov72>
2022-01-09 16:22:57 +0300

    Complete Contains Duplicate Second

---
+++ src/Contains_Duplicate_Second.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Contains_Duplicate_Second {
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int pre = map.get(nums[i]);
+                if (k >= i - pre) return true;
+                map.put(nums[i], i);
+            } else map.put(nums[i], i);
+        }
+        return false;
+//        Time Limit Exceeded
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length && j <= i + k; j++) {
+//                if (nums[i] == nums[j] && j-i <= k) return true;
+//            }
+//        }
+//        return false;
+    }
+
+    public static void main(String[] args) {
+        containsNearbyDuplicate(new int[]{1, 0, 1, 1}, 1);
+    }
+}
