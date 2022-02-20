bcd6efae9fb5d1e18cbf03b9d73266497b7aafea
Sarvar Masharipov <sarvarmasharipov72>
2022-02-20 21:44:41 +0300

    not Finished

---
+++ src/ThirdMaximumNumber.java
@@ -0,0 +1,16 @@
+public class ThirdMaximumNumber {
+    public static int thirdMax(int[] nums) {
+        int maxFirst = 0;
+        int maxSecond = 0;
+        int maxThird = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(thirdMax(new int[] {3, 7, 6}));
+    }
+}
