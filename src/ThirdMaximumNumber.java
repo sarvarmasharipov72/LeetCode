8f7b5c7e2d7a11c251fe30e836f95fb703a0057f
Sarvar Masharipov <sarvarmasharipov72>
2022-02-28 15:23:09 +0300

    complete

--- src/ThirdMaximumNumber.java
+++ src/ThirdMaximumNumber.java
@@ -1,16 +1,46 @@
 public class ThirdMaximumNumber {
     public static int thirdMax(int[] nums) {
-        int maxFirst = 0;
-        int maxSecond = 0;
-        int maxThird = 0;
 
-        for (int i = 0; i < nums.length; i++) {
+        int f = Integer.MIN_VALUE;
+        int s = Integer.MIN_VALUE;
+        int t = Integer.MIN_VALUE;
 
+        for (int i : nums) {
+            f = Math.max(i, f);
         }
-        return 1;
+        for (int i : nums) {
+            if (f == i)
+                continue;
+            s = Math.max(i, s);
+        }
+        var thirdFlase = false;
+        for (int i : nums) {
+            if (f == i || s == i)
+                continue;
+            t = Math.max(t, i);
+            thirdFlase = true;
+        }
+        return thirdFlase ? t : f;
+//        gave an error in numbers with a minus sign
+//        var hashSet = new HashSet<Integer>();
+//        for (int n : nums) {
+//            hashSet.add(n);
+//        }
+//        if (hashSet.size() == 1) {
+//            return (Integer) hashSet.toArray()[0];
+//        }
+//        if (hashSet.size() == 2) {
+//            return (Integer) hashSet.toArray()[1];
+//        }
+//        int[] num = new int[hashSet.size()];
+//        int i = 0;
+//        for (int n : hashSet) {
+//            num[i++] = n;
+//        }
+//        return num[num.length - 3];
     }
 
     public static void main(String[] args) {
-        System.out.println(thirdMax(new int[] {3, 7, 6}));
+        System.out.println(thirdMax(new int[]{3, 7, 6, 3, 4, -5, 6}));
     }
 }
