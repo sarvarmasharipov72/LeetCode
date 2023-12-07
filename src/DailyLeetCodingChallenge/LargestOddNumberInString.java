ec8a59f6a8a596c70f3850d560799e95743fd907
Sarvar Masharipov <63916119+sarvarmasharipov72@users.noreply.github.com>
2023-12-07 17:20:23 +0500

    complete some daily challenge

---
+++ src/DailyLeetCodingChallenge/LargestOddNumberInString.java
@@ -0,0 +1,12 @@
+package DailyLeetCodingChallenge;
+
+public class LargestOddNumberInString {
+    public String largestOddNumber(String num) {
+        for(int i = num.length() - 1; i >= 0; i--) {
+            if ((num.charAt(i) - 48) % 2 != 0) {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+}
